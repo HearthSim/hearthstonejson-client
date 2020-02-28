e98c4073ab0a9e7f8c5a2ca1ae757dca9332c87e
Julian Graham <julian@hearthsim.net>
2020-02-28 16:46:55 -0500

    Remove StorageBackend API

--- src/HearthstoneJSON.ts
+++ src/HearthstoneJSON.ts
@@ -1,31 +1,12 @@
 import "node-fetch";
-import { StorageBackend } from "./StorageBackend";
-import NoOpStorageBackend from "./NoOpStorageBackend";
-import LocalStorageBackend from "./LocalStorageBackend";
-import CacheProxy from "./CacheProxy";
-import { REVISIONS } from "./constants";
 import { Build, BuildNumber, Locale } from "./types";
 import { CardData } from "hearthstonejson-client";
 
 export default class HearthstoneJSON {
-	public storage: StorageBackend;
-	public storagePrefix = "hsjson-";
-
 	public endpoint = "https://api.hearthstonejson.com/v1/";
 	public defaultLocale = "enUS";
 
-	public cached: boolean | null = null;
 	public fallback: boolean | null = null;
-
-	constructor(storage?: StorageBackend) {
-		if (storage === null) {
-			this.storage = new NoOpStorageBackend();
-		} else if (typeof storage === "undefined") {
-			this.storage = new CacheProxy(new LocalStorageBackend());
-		} else {
-			this.storage = storage;
-		}
-	}
 
 	public createUrl = (build: Build, locale: Locale): string => {
 		return this.endpoint + build + "/" + locale + "/cards.json";
@@ -52,7 +33,7 @@ export default class HearthstoneJSON {
 		}
 		const _locale = locale ? locale : this.defaultLocale;
 		this.fallback = false;
-		return this.getSpecificBuild(build, _locale).catch(() => {
+		return this.fetchSpecificBuild(build, _locale).catch(() => {
 			this.fallback = true;
 			return this.fetchLatestBuild(_locale);
 		});
@@ -66,64 +47,28 @@ export default class HearthstoneJSON {
 		return this.fetchLatestBuild(locale);
 	}
 
-	protected getSpecificBuild(
-		build: BuildNumber,
-		locale: Locale
-	): Promise<CardData[]> {
-		const key = this.generateKey(build, locale);
-		let bypassCache = false;
-		if (this.storage.has(key)) {
-			const stored = this.storage.get(key);
-			// verify format
-			if (
-				typeof stored === "object" &&
-				typeof stored["revision"] === "number" &&
-				Array.isArray(stored["cards"])
-			) {
-				if (stored["revision"] >= this.getRevision(build)) {
-					this.cached = true;
-					return Promise.resolve(stored["cards"]);
-				}
-			}
-			// local version is not valid or outdated, do a full reload
-			bypassCache = true;
-		}
-		this.cached = false;
-		return this.fetchSpecificBuild(build, locale, bypassCache).catch(error => {
-			// possibly invalid CORS header in cache
-			return this.fetchSpecificBuild(build, locale, true);
-		});
-	}
-
 	protected fetchSpecificBuild(
 		build: BuildNumber,
-		locale: Locale,
-		bypassCache?: boolean
+		locale: Locale
 	): Promise<CardData[]> {
 		const headers = new Headers();
 		headers.set("accept", "application/json; charset=utf-8");
 		return fetch(this.createUrl(build, locale), {
 			method: "GET",
 			mode: "cors",
-			cache: bypassCache ? "reload" : "default",
 			headers,
-		})
-			.then((response: Response): Promise<CardData[]> => {
-				const statusCode = response.status;
-				if (statusCode !== 200) {
-					throw new Error("Expected status code 200, got " + statusCode);
-				}
-				return response.json();
-			})
-			.then((payload: CardData[]) => {
-				this.store(build, locale, payload);
-				return payload;
-			});
+		}).then((response: Response): Promise<CardData[]> => {
+			const statusCode = response.status;
+			if (statusCode !== 200) {
+				throw new Error("Expected status code 200, got " + statusCode);
+			}
+			return response.json();
+		});
 	}
 
 	protected fetchLatestBuild(locale: Locale): Promise<CardData[]> {
 		return this.fetchLatestBuildNumber(locale).then((build: BuildNumber) =>
-			this.getSpecificBuild(build, locale)
+			this.fetchSpecificBuild(build, locale)
 		);
 	}
 
@@ -147,38 +92,5 @@ export default class HearthstoneJSON {
 			const buildNumber = +build;
 			return buildNumber;
 		});
-	}
-
-	protected store(
-		buildNumber: BuildNumber,
-		locale: Locale,
-		payload: CardData[]
-	): void {
-		if (!payload.length) {
-			// this doesn't look right - refuse to cache this
-			return;
-		}
-		const key = this.generateKey(buildNumber, locale);
-
-		this.storage.set(key, {
-			revision: this.getRevision(buildNumber),
-			cards: payload,
-		});
-	}
-
-	protected generateKey(build: Build, locale: Locale): string {
-		if (build === "latest") {
-			throw new Error('Refusing to generate key for "latest" metadata');
-		}
-		return this.storagePrefix + build + "_" + locale;
-	}
-
-	protected getRevision(build: BuildNumber): number {
-		const _build = "" + build;
-		let revision = 0;
-		if (typeof REVISIONS[_build] === "number") {
-			revision = REVISIONS[_build];
-		}
-		return revision;
 	}
 }
